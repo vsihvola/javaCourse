6493da84bf062a752fcf305ab809f19deda630aa
vsihvola <57141359+vsihvola@users.noreply.github.com>
2019-10-30 22:41:38 +0200

    Update Ajoneuvo.java

--- Ajoneuvo.java
+++ Ajoneuvo.java
@@ -1,8 +1,52 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+Information about the "Ajoneuvo" task. In task I had to make program including a class. In assigment
+only information what was given was what the program have to print in what order.
+*/
+
+class Auto extends Ajoneuvo {
+
+    boolean kaynnissa;
+
+    String merkki, malli, rekisteri_nro;
+
+    public Auto(int paino, int huippunopeus, int ajetut_kilometrit, String merkki, String malli, String rekisteri_nro, boolean kaynnissa) {
+        super(paino, huippunopeus, ajetut_kilometrit);
+        this.merkki = merkki;
+        this.malli = malli;
+        this.rekisteri_nro = rekisteri_nro;
+
+    }
+
+    public void kaynnista() {
+        kaynnissa = true;
+
+    }
+
+    public void asetaPaino(int uusi_paino) {
+
+    }
+
+    public void sammuta(boolean kaynnissa) {
+        kaynnissa = false;
+    }
+
+    public void katsasta() {
+        if (kaynnissa == true) {
+            System.out.println("Auton tiedot:" + "\nMerkki: " + this.merkki + "\nMalli: " + this.malli + "\nAjokilometrit: "
+                    + super.anna_ajetut_kilometrit() + "\nPaino (kg): " + super.anna_paino() + "\nHuippunopeus (km/h): " + super.anna_huippunopeus()
+                    + "\nRekisterinumero: " + rekisteri_nro + "\nAuto on käynnissä");
+
+        } else {
+            System.out.println("Auton tiedot:" + "\nMerkki: " + this.merkki + "\nMalli: " + this.malli + "\nAjokilometrit: "
+                    + super.anna_ajetut_kilometrit() + "\nPaino (kg): " + super.anna_paino() + "\nHuippunopeus (km/h): " + super.anna_huippunopeus()
+                    + "\nRekisterinumero: " + rekisteri_nro + "\nAuto ei ole käynnissä");
+        }
+    }
+
+}
+
+/*--------------------------*/
+
 package ajoneuvo;
 
 import java.io.*;
